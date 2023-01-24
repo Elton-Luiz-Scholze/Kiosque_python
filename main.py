d03d6d4a759299a1b923f3d33d2130c5e4c94161
Elton Luiz Scholze <elscholze@gmail.com>
2023-01-24 18:24:02 -0300

    feat: created function to list product by type

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from management.product_handler import get_product_by_id
+from management.product_handler import get_product_by_id, get_products_by_type
 
 if __name__ == "__main__":
     print(get_product_by_id(25))
     print(get_product_by_id(0))
+    print(get_products_by_type("fruit"))
+    print(get_products_by_type("codewars"))
