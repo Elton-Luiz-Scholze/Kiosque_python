74dbb61138bb84a0e09748e6d23aba7a3c0623ce
Elton Luiz Scholze <elscholze@gmail.com>
2023-01-27 15:00:03 -0300

    feat: created function calculate_tab

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from management.product_handler import get_product_by_id, get_products_by_type, add_product
+from management.tab_handler import calculate_tab
 from menu import products
 
 new_product = {
@@ -9,9 +10,23 @@ new_product = {
     "type": "drink",
 }
 
-if __name__ == "__main__":
-    # print(get_product_by_id(25))
-    # print(get_product_by_id(0))
-    # print(get_products_by_type("fruit"))
-    # print(get_products_by_type("codewars"))
+table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
+table_2 = [
+    {"_id": 10, "amount": 3},
+    {"_id": 20, "amount": 2},
+    {"_id": 21, "amount": 5},
+]
+
+
+def main():
+    print(get_product_by_id(25))
+    print(get_product_by_id(0))
+    print(get_products_by_type("fruit"))
+    print(get_products_by_type("codewars"))
     print(add_product(products, **new_product))
+    print(calculate_tab(table_1))
+    print(calculate_tab(table_2))
+
+
+if __name__ == "__main__":
+    main()
